33b8d9cb6990f34d1366e177daee54347d6e75ec
mlozier <mlozier@users.noreply.github.com>
2023-03-11 16:42:27 -0800

    asdf

--- testris.py
+++ testris.py
@@ -22,4 +22,26 @@ board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 tetriminoT = [[1, 1, 1],
               [0, 1, 0]]
 
-print("Hello World!")
+def printBoard(board):
+    for i in range(20):
+        for j in range(10):
+            print (board[i][j], end="")
+        print ("\n")
+
+printBoard(board)
+
+def moveTetrimino(tetrimino, board, col, row):
+    for i in range (2):
+        for j in range (3):
+            #print("i is: ", end="")
+            #print(i)
+            #print("j is: ", end = "")
+            #print(j)
+            #print(" i plus col is: ", end="")
+            #print(i + col)           
+            board[row + i][col + j] = tetrimino[i][j]
+
+moveTetrimino(tetriminoT, board, 1, 0)
+
+printBoard(board)
+
